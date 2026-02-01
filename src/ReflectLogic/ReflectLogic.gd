3f720f621fc6be2d424eede7e5f30b482cca20c7
abderrahman504 <rahmankhsherb@gmail.com>
2026-02-01 12:27:08 +0200

    Restrict reflection normal vector to the 4 cardinals

--- src/ReflectLogic/ReflectLogic.gd
+++ src/ReflectLogic/ReflectLogic.gd
@@ -8,5 +8,17 @@ class_name ReflectLogic
 ## [code]collision[/code] is the collision information from the colliding body.
 func get_reflect_angle(impact : Vector2, collision : KinematicCollision2D) -> float:
 	var norm = collision.get_normal()
+	# Clamp the norm to the closest cardinal direction
+	var closest : Vector2 = Vector2.LEFT
+	print("Unclamped norm = ", norm)
+	if norm.dot(Vector2.UP) > norm.dot(closest):
+		closest = Vector2.UP
+	if norm.dot(Vector2.RIGHT) > norm.dot(closest):
+		closest = Vector2.RIGHT
+	if norm.dot(Vector2.DOWN) > norm.dot(closest):
+		closest = Vector2.DOWN
+	
+	norm = closest
+	print("Clamped norm = ", norm)
 	var reflect_angle = impact.bounce(norm).angle()
 	return reflect_angle
